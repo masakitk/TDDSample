5345f5a19100b04dd036f5745c0b720c1a7ffaab
masaki_takahashi <masakitk@gmail.com>
2011-12-19 01:52:15 +0900

    fixed misspelling

--- TddFizzBuzz/FizzBuzzForm.Designer.cs
+++ TddFizzBuzz/FizzBuzzForm.Designer.cs
@@ -28,20 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.maxNumberTextbox = new System.Windows.Forms.TextBox();
+            this.maxNumberTextBox = new System.Windows.Forms.TextBox();
             this.fizzBuzzButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.fizzBuzzDataGirdView1 = new TddFizzBuzz.FizzBuzzDataGirdView();
-            ((System.ComponentModel.ISupportInitialize)(this.fizzBuzzDataGirdView1)).BeginInit();
+            this.fizzBuzzDataGridView = new TddFizzBuzz.FizzBuzzDataGirdView();
+            ((System.ComponentModel.ISupportInitialize)(this.fizzBuzzDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // maxNumberTextbox
+            // maxNumberTextBox
             // 
-            this.maxNumberTextbox.Location = new System.Drawing.Point(47, 14);
-            this.maxNumberTextbox.Name = "maxNumberTextbox";
-            this.maxNumberTextbox.Size = new System.Drawing.Size(33, 19);
-            this.maxNumberTextbox.TabIndex = 0;
+            this.maxNumberTextBox.Location = new System.Drawing.Point(47, 14);
+            this.maxNumberTextBox.Name = "maxNumberTextBox";
+            this.maxNumberTextBox.Size = new System.Drawing.Size(33, 19);
+            this.maxNumberTextBox.TabIndex = 0;
             // 
             // fizzBuzzButton
             // 
@@ -70,28 +70,28 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "まで";
             // 
-            // fizzBuzzDataGirdView1
+            // fizzBuzzDataGridView
             // 
-            this.fizzBuzzDataGirdView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.fizzBuzzDataGirdView1.Location = new System.Drawing.Point(12, 39);
-            this.fizzBuzzDataGirdView1.Name = "fizzBuzzDataGirdView1";
-            this.fizzBuzzDataGirdView1.RowTemplate.Height = 21;
-            this.fizzBuzzDataGirdView1.Size = new System.Drawing.Size(240, 211);
-            this.fizzBuzzDataGirdView1.TabIndex = 4;
+            this.fizzBuzzDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.fizzBuzzDataGridView.Location = new System.Drawing.Point(12, 39);
+            this.fizzBuzzDataGridView.Name = "fizzBuzzDataGridView";
+            this.fizzBuzzDataGridView.RowTemplate.Height = 21;
+            this.fizzBuzzDataGridView.Size = new System.Drawing.Size(240, 211);
+            this.fizzBuzzDataGridView.TabIndex = 4;
             // 
             // FizzBuzzForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.fizzBuzzDataGirdView1);
+            this.Controls.Add(this.fizzBuzzDataGridView);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.fizzBuzzButton);
-            this.Controls.Add(this.maxNumberTextbox);
+            this.Controls.Add(this.maxNumberTextBox);
             this.Name = "FizzBuzzForm";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.fizzBuzzDataGirdView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fizzBuzzDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,11 +99,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox maxNumberTextbox;
+        private System.Windows.Forms.TextBox maxNumberTextBox;
         private System.Windows.Forms.Button fizzBuzzButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private FizzBuzzDataGirdView fizzBuzzDataGirdView1;
+        private FizzBuzzDataGirdView fizzBuzzDataGridView;
     }
 }
 
