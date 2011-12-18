cb1d1bd7693136bc1b94e2deaf03e74def4678ad
masaki_takahashi <masakitk@gmail.com>
2011-12-19 01:46:41 +0900

    written test

---
+++ TddFizzBuzzTest/FizzBuzzFormTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using NUnit.Extensions.Forms;
+using NUnit.Framework;
+using TddFizzBuzz;
+
+namespace TddFizzBuzzTest
+{
+    [TestFixture]
+    public class TddFizzBuzzTest
+    {
+        [Test]
+        public void Test01_10までのFizzBuzz結果の確認()
+        {
+            var target = new FizzBuzzForm();
+            target.Show();
+
+            new TextBoxTester("maxNumberTextBox", target).Enter("10");
+            new ButtonTester("fizzBuzzButton", target).Click();
+            var dataGrid = new Finder<DataGridView>("fizzBuzzDataGridView", target).Find();
+            AssertForOneRow(dataGrid, 1, "1");
+        }
+
+        private static void AssertForOneRow(DataGridView dataGrid, int rowIndex, string expected)
+        {
+            Assert.That(dataGrid["FizzBuzzValue", rowIndex].Value, Is.EqualTo(expected));
+        }
+    }
+}
